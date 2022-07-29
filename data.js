eaf1c0d698e7b93ff7552ea63fd93bc21e0d7a7c
Daniel Peprah <peprahdaniel.dp@gmail.com>
2022-07-28 19:30:38 -0800

    modified columns path

--- data.js
+++ data.js
@@ -1,43 +1,43 @@
-import { BellIcon, ClipboardCheckIcon, CogIcon, FolderIcon, UsersIcon } from "@heroicons/react/outline";
+import {
+  BellIcon,
+  ClipboardCheckIcon,
+  CogIcon,
+  FolderIcon,
+  UsersIcon,
+} from "@heroicons/react/outline";
 
 export const mainData = [
-    {
-        title:"Projects",
-        icon:<FolderIcon className="w-8 h-8"/>,
-        iconColor:"#ffffff",
-        iconBg:"#398acc",
-        count:20,
-        link:'/app/projects/'
-    },
-    {
-        title:"Tasks",
-        icon:<ClipboardCheckIcon className="w-8 h-8"/>,
-        iconColor:"#ffffff",
-        iconBg:"#e95656",
-        count:101,
-        link:'/app/tasks/'
-    },
-    {
-        title:"Members",
-        icon:<UsersIcon className="w-8 h-8"/>,
-        iconColor:"#ffffff",
-        iconBg:"green",
-        count:10,
-        link:'/app/members/'
-    },
-   
+  {
+    title: "Projects",
+    icon: <FolderIcon className="w-8 h-8" />,
+    iconColor: "#ffffff",
+    iconBg: "#398acc",
+    count: 20,
+    link: "/projects/",
+  },
+  {
+    title: "Tasks",
+    icon: <ClipboardCheckIcon className="w-8 h-8" />,
+    iconColor: "#ffffff",
+    iconBg: "#e95656",
+    count: 101,
+    link: "/tasks/",
+  },
+  {
+    title: "Members",
+    icon: <UsersIcon className="w-8 h-8" />,
+    iconColor: "#ffffff",
+    iconBg: "green",
+    count: 10,
+    link: "/members/",
+  },
 
-    {
-        title:"Notifications",
-        icon:<BellIcon className="w-8 h-8"/>,
-        iconColor:"#ffffff",
-        iconBg:"orange",
-        count:10,
-        link:'/app/notifications/'
-    },
-   
-    
-
-
-
-]
+  {
+    title: "Notifications",
+    icon: <BellIcon className="w-8 h-8" />,
+    iconColor: "#ffffff",
+    iconBg: "orange",
+    count: 10,
+    link: "/notifications/",
+  },
+];
